af242428c8d416533fc3d397a4b5d0d08228a7f7
Peli de Halleux <pelikhan@users.noreply.github.com>
2020-06-18 12:46:33 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -40,7 +40,7 @@ c b d d d d d 5 5 5 5 5 5 5 b .
 . . c b d d d d d 5 5 5 b b . . 
 . . . c c c c c c c c b b . . . 
 `, SpriteKind.Player)
-mySprite.setFlag(SpriteFlag.BounceOnWall, false)
+mySprite.setFlag(SpriteFlag.BounceOnWall, true)
 controller.moveSprite(mySprite)
 class Stuff {
 }
